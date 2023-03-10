9fcf73c249c08c327eb3b9d5f6226ee0da676b0b
psj1123 <jin1123aa@gmail.com>
2023-03-10 17:12:05 +0900

    [Fix] 프로젝트 리스트 경로 변경, 불필요한 border 삭제

--- src/Components/Listpage/Listpage.js
+++ src/Components/Listpage/Listpage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import Managedlist from './Managedlist';
 import Participatinglist from './Participatinglist';
@@ -13,8 +13,19 @@ function Listpage(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false); // 프로젝트 추가 모달 State
   const [modalIsOpen1, setModalIsOpen1] = useState(false); // 프로젝트 참여 모달 State
 
-  let [list, setList] = useState(data); // 생성된 프로젝트 데이터를 다루는 State
+  let [list, setList] = useState([]); // 생성된 프로젝트 데이터를 다루는 State
   let [nextNum, setNextNum] = useState(1); // 생성된 프로젝트 데이터의  고유한 ID 부여를 위한 State
+
+  let [listBorder, setListBorder] = useState('');
+  useEffect(() => {
+    // 프로젝트 테두리 border 컨트롤
+    if (list.length === 0) {
+      setListBorder('');
+    } else {
+      setListBorder('listBorderTop');
+    }
+  });
+
   return (
     <div className="listSection" align="center">
       {/* --------- 상단 , 관리중인 프로젝터 --------- */}
@@ -22,7 +33,7 @@ function Listpage(props) {
         <h4>관리중인 프로젝트</h4>
       </div>
       <div>
-        <Container className="listBorderTop">
+        <Container className={listBorder}>
           <Row>
             {list.map(function (a, i) {
               return <Managedlist key={i} num={i} list={list} con={a} />;
