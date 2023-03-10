e2badfdc97994774c82c9482f99a5f507f8013ee
psj1123 <jin1123aa@gmail.com>
2023-03-10 10:54:09 +0900

    [Add] index.js 라이브러리

--- src/index.js
+++ src/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Register from './Pages/Register';
+import MyProjectsList from './Pages/MyProjectsList';
+import Project from './Pages/Project';
+import NotFound from './Pages/NotFound';
 import { UserDataProvider } from './Contexts/UserData';
-import ReactModal from 'react-modal';
-import App from './App';
-
-ReactModal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <UserDataProvider>
-      <App />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/myprojectslist" element={<MyProjectsList />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/project/:projectcode" element={<Project />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </UserDataProvider>
   </BrowserRouter>
 );
