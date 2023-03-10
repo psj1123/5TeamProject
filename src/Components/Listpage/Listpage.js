a8f347552e665fa182589f5668e10c57ea752b41
psj1123 <jin1123aa@gmail.com>
2023-03-10 17:56:26 +0900

    [Fix] 프로젝트 리스트 페이지 디자인 1차 공유

--- src/Components/Listpage/Listpage.js
+++ src/Components/Listpage/Listpage.js
@@ -8,6 +8,7 @@ import '../ListPageModals/ProjectListModal.css';
 import '../../Styles/Cards.css';
 import ProjectListModalJoin from '../ListPageModals/ProjectListModalJoin';
 import data from './data';
+import './list.css';
 
 function Listpage(props) {
   const [modalIsOpen, setModalIsOpen] = useState(false); // 프로젝트 추가 모달 State
@@ -27,103 +28,110 @@ function Listpage(props) {
   });
 
   return (
-    <div className="listSection" align="center">
-      {/* --------- 상단 , 관리중인 프로젝터 --------- */}
-      <div align="center" className="top">
-        <h4>관리중인 프로젝트</h4>
-      </div>
-      <div>
-        <Container className={listBorder}>
-          <Row>
-            {list.map(function (a, i) {
-              return <Managedlist key={i} num={i} list={list} con={a} />;
-            })}
-          </Row>
-        </Container>
-      </div>
-      {/* --------- 상단 , 관리중인 프로젝터 --------- */}
-      {/* --------- 하단 , 참여중인 프로젝터 --------- */}
-      <div align="center" style={{ marginTop: '20px' }}>
-        <h4>참여중인 프로젝트</h4>
-      </div>
-      <div>
-        <Container className="listBorderTop">
-          <Row>
-            {list.map(function (a, i) {
-              return <Participatinglist key={i} num={i} list={list} con={a} />;
-            })}
-            <Col sm={2}>
-              <Card
-                bg={'Light'.toLowerCase()}
-                style={{ width: '11rem' }}
-                className="mb-2 addCard"
-              >
-                <div>
-                  <Card.Header align="center" className="header">
-                    프로젝트 추가
-                  </Card.Header>
-                  <Card.Body align="center">
-                    {/* ----- 프로젝트 [추가] 모달창 구현 , 버튼 ----- */}
-                    <div className="addModalh">
-                      <Button
-                        className="btn_add"
-                        variant="outline-secondary"
-                        onClick={() => setModalIsOpen(true)}
-                      >
+    <section>
+      <div className="listSection" align="center">
+        {/* --------- 상단 , 관리중인 프로젝터 --------- */}
+        <div className="Project_Container">
+          <div>
+            <h4>관리중인 프로젝트</h4>
+          </div>
+          <div className="top_Project">
+            <Container className={listBorder}>
+              <Row>
+                {list.map(function (a, i) {
+                  return <Managedlist key={i} num={i} list={list} con={a} />;
+                })}
+              </Row>
+            </Container>
+          </div>
+        </div>
+        {/* --------- 하단 , 참여중인 프로젝터 --------- */}
+        <div className="Project_Container">
+          <div className="Project_Name">
+            <h4>프로젝트 생성</h4>
+          </div>
+          <div>
+            <Container className="listBorderTop">
+              <Row>
+                {list.map(function (a, i) {
+                  return (
+                    <Participatinglist key={i} num={i} list={list} con={a} />
+                  );
+                })}
+                <Col sm={2}>
+                  <Card
+                    bg={'Light'.toLowerCase()}
+                    style={{ width: '11rem' }}
+                    className="mb-2 addCard"
+                  >
+                    <div className="projectBlock">
+                      <Card.Header align="center" className="header">
                         프로젝트 추가
-                      </Button>
-                      <Modal
-                        className="addModal addmodalBody"
-                        isOpen={modalIsOpen}
-                        onRequestClose={() => setModalIsOpen(false)}
-                      >
-                        <div>
-                          <ProjectListModalAdd
-                            setModalIsOpen={setModalIsOpen}
-                            list={list}
-                            setList={setList}
-                            nextNum={nextNum}
-                            setNextNum={setNextNum}
-                          />
+                      </Card.Header>
+                      <Card.Body align="center">
+                        {/* ----- 프로젝트 [추가] 모달창 구현 , 버튼 ----- */}
+                        <div className="addModalh">
+                          <Button
+                            className="btn_add"
+                            variant="outline-secondary"
+                            onClick={() => setModalIsOpen(true)}
+                          >
+                            프로젝트 추가
+                          </Button>
+                          <Modal
+                            className="addModal addmodalBody"
+                            isOpen={modalIsOpen}
+                            onRequestClose={() => setModalIsOpen(false)}
+                          >
+                            <div className="projectAdd">
+                              <ProjectListModalAdd
+                                setModalIsOpen={setModalIsOpen}
+                                list={list}
+                                setList={setList}
+                                nextNum={nextNum}
+                                setNextNum={setNextNum}
+                              />
+                            </div>
+                          </Modal>
+                          {/* ----- 프로젝트 [추가] 모달창 구현 ----- */}
                         </div>
-                      </Modal>
-                      {/* ----- 프로젝트 [추가] 모달창 구현 ----- */}
-                    </div>
-                    {/* ----- 프로젝트 [참여] 모달창 구현 , 버튼 ----- */}
-                    <div className="addModalh1">
-                      <Button
-                        className="btn_join"
-                        variant="outline-secondary"
-                        onClick={() => setModalIsOpen1(true)}
-                      >
-                        프로젝트 참여
-                      </Button>
-                      <Modal
-                        className="joinModal joinmodalBody"
-                        isOpen={modalIsOpen1}
-                        onRequestClose={() => setModalIsOpen1(false)}
-                      >
-                        <div>
-                          <ProjectListModalJoin
-                            setModalIsOpen={setModalIsOpen1}
-                            list={list}
-                            setList={setList}
-                            nextNum={nextNum}
-                            setNextNum={setNextNum}
-                          />
+                        {/* ----- 프로젝트 [참여] 모달창 구현 , 버튼 ----- */}
+                        <div className="addModalh1">
+                          <Button
+                            className="btn_join"
+                            variant="outline-secondary"
+                            onClick={() => setModalIsOpen1(true)}
+                          >
+                            프로젝트 참여
+                          </Button>
+                          <Modal
+                            className="joinModal joinmodalBody"
+                            isOpen={modalIsOpen1}
+                            onRequestClose={() => setModalIsOpen1(false)}
+                          >
+                            <div>
+                              <ProjectListModalJoin
+                                setModalIsOpen={setModalIsOpen1}
+                                list={list}
+                                setList={setList}
+                                nextNum={nextNum}
+                                setNextNum={setNextNum}
+                              />
+                            </div>
+                          </Modal>
                         </div>
-                      </Modal>
+                        {/* ----- 프로젝트 [참여] 모달창 구현 ----- */}
+                      </Card.Body>
                     </div>
-                    {/* ----- 프로젝트 [참여] 모달창 구현 ----- */}
-                  </Card.Body>
-                </div>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+                  </Card>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </div>
+        {/* --------- 하단 , 참여중인 프로젝터 --------- */}
       </div>
-      {/* --------- 하단 , 참여중인 프로젝터 --------- */}
-    </div>
+    </section>
   );
 }
 
