17e801238b55b4a02872ce3f3e72a5bda0390a87
dlrdyd97 <dlrdyd97@naver.com>
2023-03-10 12:14:36 +0900

    테스트제거

--- src/Components/Listpage/data.js
+++ src/Components/Listpage/data.js
@@ -1,12 +1,2 @@
-let data = [
-  // 생성된 프로젝트 데이터
-  {
-    id: 100, // 페이지 이동을 위한 프로젝트 고유 ID
-    projectName: 'test', // 프로젝트 이름
-    content: 'test', // 프로젝트 내용
-    deadline: '2023-12-31', // 마감일
-    management: 1, // 유저가 생성한 페이지면 1 , 아니면 0
-    join: 1, // 유저가 참여중인 페이지면 1 , 아니면 0
-  },
-];
+let data = [{}];
 export default data;
