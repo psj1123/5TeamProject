481a4e2bf92cb9688a5366510fb9564f4fdd1ca7
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-03-19 02:11:54 +0900

    브랜치 테스트

--- src/Components/Projects/ProjectPostDetail.js
+++ src/Components/Projects/ProjectPostDetail.js
@@ -9,47 +9,57 @@ const ProjectPostDetail = ({
   postDelete,
 }) => {
   return (
-    <div className="postBox">
-      <div className="postTitleBox">{nowPost.postTitle}</div>
-      <div className="postContentBox">{nowPost.postContent}</div>
-      <div className="postWriterBox">{nowPost.postWriter}</div>
-      <div className="postedDateBox">{nowPost.postedDate}</div>
-      <button
-        onClick={() => {
-          if (state.email === nowPost.writerEmail) {
-            setIsPostUpdating(true);
-          } else {
-            alert('작성자만 수정이 가능합니다.');
-          }
-        }}
-      >
-        수정
-      </button>
-      <button
-        onClick={() => {
-          if (
-            state.email === nowPost.writerEmail ||
-            state.email === projectInfo.email
-          ) {
-            if (window.confirm('정말 삭제하시겠습니까?')) {
-              postDelete();
+    <div className="postVeiwBox">
+      <div className="postTitleBox">
+        <div>{nowPost.postTitle}</div>
+      </div>
+      <div className="postWriterBox">
+        <div>{nowPost.postWriter}</div>
+      </div>
+      <div className="postedDateBox">
+        <div>{nowPost.postedDate}</div>
+      </div>
+      <div className="postContentBox">
+        <div>{nowPost.postContent}</div>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            if (state.email === nowPost.writerEmail) {
+              setIsPostUpdating(true);
             } else {
-              return;
+              alert('작성자만 수정이 가능합니다.');
             }
-          } else {
-            alert('작성자 또는 관리자만 삭제가 가능합니다.');
-          }
-        }}
-      >
-        삭제
-      </button>
-      <button
-        onClick={() => {
-          setIsPostOpened(false);
-        }}
-      >
-        목록으로 돌아가기
-      </button>
+          }}
+        >
+          수정
+        </button>
+        <button
+          onClick={() => {
+            if (
+              state.email === nowPost.writerEmail ||
+              state.email === projectInfo.email
+            ) {
+              if (window.confirm('정말 삭제하시겠습니까?')) {
+                postDelete();
+              } else {
+                return;
+              }
+            } else {
+              alert('작성자 또는 관리자만 삭제가 가능합니다.');
+            }
+          }}
+        >
+          삭제
+        </button>
+        <button
+          onClick={() => {
+            setIsPostOpened(false);
+          }}
+        >
+          목록으로 돌아가기
+        </button>
+      </div>
     </div>
   );
 };
