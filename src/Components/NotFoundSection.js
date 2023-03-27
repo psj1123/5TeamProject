91ecc5ef80e31c4863cb5222b3b0be9c7c7b9d41
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-03-28 07:10:55 +0900

    [FIX] 이미지 경로 수정

--- src/Components/NotFoundSection.js
+++ src/Components/NotFoundSection.js
@@ -3,18 +3,16 @@ import { Link } from 'react-router-dom';
 import '../Styles/NotFound.css';
 
 const NotFoundSection = () => {
-    return (
-        <section>
-            <div className="Box404">
-                <img className="image404" src="../../img/404.png" alt="404 error" />
-            </div>
-            <Link to="/">
-                <div className="home">
-                    홈으로 가기
-                </div>
-            </Link>
-        </section>
-    );
+  return (
+    <section>
+      <div className="Box404">
+        <img className="image404" src="/img/404.png" alt="404 error" />
+      </div>
+      <Link to="/">
+        <div className="home">홈으로 가기</div>
+      </Link>
+    </section>
+  );
 };
 
 export default NotFoundSection;
